2f9ee6348703487bd794439807e86de227e093eb
GedIra <irankundag65@gmail.com>
2025-06-30 11:52:30 +0200

    Completion of Convert a Number to a String Kata

---
+++ 8kyu/Convert_a_Number_to_a_String.js
@@ -0,0 +1,9 @@
+/** 
+ * numberToString = num => num.toString()
+ * numberToString = num => String(num)
+ * numberToString = num => `${num}`
+*/
+numberToString = num => ""+num
+
+console.log(typeof numberToString(9))
+//String
