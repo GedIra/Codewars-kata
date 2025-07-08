db128c6e90e548506f67cbfc7554b1c1ea90134c
GedIra <irankundag65@gmail.com>
2025-07-08 15:51:12 +0200

    Completion of Function 1 hello world

---
+++ 8kyu/Beginner_Series_2_Clock.js
@@ -0,0 +1,5 @@
+/**
+ * Link: https://www.codewars.com/kata/55f9bca8ecaa9eac7100004a/train/javascript
+ */
+
+past = (h, m, s) => 1000*(h*3600 + m*60 + s)
---
+++ 8kyu/Function_1-hello_world.js
@@ -0,0 +1,9 @@
+/**
+ * Link: https://www.codewars.com/kata/523b4ff7adca849afe000035/train/javascript
+ * Write a function "greet" that returns "hello world!"
+*/
+
+const greet = _ => 
+    String.fromCharCode(104, 101, 108, 108, 111, 32, 119, 111, 114, 108, 100, 33)
+
+console.log(greet)
