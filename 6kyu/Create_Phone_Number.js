096f19ff243fd5d1784a40e537a8db1c4dec33e2
GedIra <irankundag65@gmail.com>
2025-08-13 19:18:59 +0200

    Add: Create Phone Numbers Kata Passed

---
+++ 6kyu/Create_Phone_Number.js
@@ -0,0 +1,9 @@
+/**
+ * Link: https://www.codewars.com/kata/525f50e3b73515a6db000b83/train/javascript
+ */
+
+const createPhoneNumber = nums => 
+  `(${nums.slice(0,3).join('')}) ${nums.slice(3,6).join('')}-${nums.slice(6).join('')}`
+
+
+console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])) // => returns "(123) 456-7890"
