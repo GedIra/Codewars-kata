5f6b032857562102b402996af7e5a32b386d3348
GedIra <irankundag65@gmail.com>
2025-10-28 17:18:56 +0200

    chore: PeteTheBaker Kata Passed

---
+++ 5kyu/PeteTheBaker.js
@@ -0,0 +1,15 @@
+function cakes(recipe, available) {
+  const lcm = []
+  for( const data of Object.keys(recipe) ){
+    if(!Object.hasOwn(available, data)){
+      return 0
+    }
+    lcm.push(Math.floor(available[data] / recipe[data]))
+  }
+  return Math.min(...lcm)
+}
+
+//Tests
+
+// console.log(cakes({flour: 500, sugar: 200, eggs: 1}, {flour: 1200, sugar: 1200, eggs: 5, milk: 200}))
+// console.log(cakes({apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100}, {sugar: 500, flour: 2000, milk: 2000}))
